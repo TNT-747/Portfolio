76c5ae32277742a2a1a3d14ad8e9698dbd4ef7a7
Abderrahmane Kassimi <kassimi.docs@gmail.com>
2026-01-03 22:33:43 +0000

    Fix TypeScript error: remove unused index parameter

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -263,7 +263,7 @@ const Projects = () => {
           animate="show"
           className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8"
         >
-          {projects.map((project, index) => (
+          {projects.map((project) => (
             <motion.div 
               key={project.id} 
               variants={item} 
